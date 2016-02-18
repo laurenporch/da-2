9272d8d79a307088a3dbae9aef30067ec9e3b17d
laurenporch <loporch@gmail.com>
2016-02-17 21:16:07 -0500

    Mostly fish movement
    Also some animation for Bruce

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ window.onload = function() {
     // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
     // You will need to change the paths you pass to "game.load.image()" or any other
     // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
+    // All loading functions will typically all be found inside "preload()".    
     
     "use strict";
     
@@ -20,9 +20,10 @@ window.onload = function() {
         game.load.image('background', 'assets/underwater-bubbles-4055.jpg');
         game.load.tilemap('tileset', 'assets/underwater-tiles.json',null,Phaser.Tilemap.TILED_JSON);
         game.load.image('tiles','assets/tiles_underwater.png');
-        game.load.spritesheet('shark', 'assets/shark-sprite.png',120,99);
+        game.load.spritesheet('shark', 'assets/shark-sprite-full.png',120,99,8);
         game.load.spritesheet('blue', 'assets/BlueFish4.png',1435, 1080);
         game.load.spritesheet('purple', 'assets/RedFish2.png', 1296, 1080);
+        game.load.audio('background_music', 'assets/263945__audiolarx__water-bubbles-03.wav');
         
     }
     
@@ -34,10 +35,16 @@ window.onload = function() {
     var purplefishies;
     var bluefish;
     var purplefish;
+    var background_music;
     
     function create() {
         // Start physics.
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        // Add audio.
+        background_music = game.add.audio('background_music');
+        background_music.loop = true;
+        background_music.play();
         
         // Create large static background and set its bounds.
         game.add.tileSprite(0,0,1920,1080,'background');
@@ -54,7 +61,10 @@ window.onload = function() {
         shark = game.add.sprite(64,64,'shark');
         shark.scale.setTo(0.5,0.5);
         shark.anchor.set(0.5);
-        shark.animations.add('right', [0,4], 10, true);
+        shark.animations.add('right', [4,7], 5, true);
+        shark.animations.add('left', [3,0], 5, true);
+        shark.animations.add('eatLeft', [2,3], 5, true);
+        shark.animations.add('eatRight', [5,4], 5, true);
 
         //  Shark physics.
         game.physics.arcade.enable(shark);
@@ -74,19 +84,66 @@ window.onload = function() {
         bluefishies.enableBody = true;
         purplefishies.enableBody = true;
 
-        // Make a blue fish!        
+        // Make a blue fish! And make it move.
         var bluefish = bluefishies.create(165,150, 'blue');
         bluefish.scale.setTo(.025,.025);
+        // This is a workaround to random movement.
+        bluefish.body.gravity.y = 200 * Math.random();
+        bluefish.body.bounce.y = 1;
+        bluefish.body.gravity.x = 200 * Math.random();
+        bluefish.body.bounce.y = 1;
         
-        // Make a purple fish!
+        // Make a purple fish! And make it move.
         var purplefish = purplefishies.create(280,290, 'purple');
         purplefish.scale.setTo(.025,.025);
+        // This is a workaround to random movement.
+        purplefish.body.gravity.y = 200 * Math.random();
+        purplefish.body.bounce.y = 1;
+        purplefish.body.gravity.x = 200 * Math.random();
+        purplefish.body.bounce.y = 1;
+        
+        // Blue fish 2.
+        var bluefish2 = bluefishies.create(1700,800, 'blue');
+        bluefish2.scale.setTo(.025,.025);
+        // This is a workaround to random movement.
+        bluefish2.body.gravity.y = -200 * Math.random();
+        bluefish2.body.bounce.y = 1;
+        bluefish2.body.gravity.x = 200 * Math.random();
+        bluefish2.body.bounce.y = 1;
+        
+        // Purple fish 2.
+        var purplefish2 = purplefishies.create(1000,500, 'purple');
+        purplefish2.scale.setTo(.025,.025);
+        // This is a workaround to random movement.
+        purplefish2.body.gravity.y = 200 * Math.random();
+        purplefish2.body.bounce.y = 1;
+        purplefish2.body.gravity.x = -200 * Math.random();
+        purplefish2.body.bounce.y = 1;
+        
+        // Blue fish 3.
+        var bluefish3 = bluefishies.create(300,1000, 'blue');
+        bluefish3.scale.setTo(.025,.025);
+        // This is a workaround to random movement.
+        bluefish3.body.gravity.y = -200 * Math.random();
+        bluefish3.body.bounce.y = 1;
+        bluefish3.body.gravity.x = -200 * Math.random();
+        bluefish3.body.bounce.y = 1;
+                
+        // Purple fish 3.
+        var purplefish3 = purplefishies.create(1300,400, 'purple');
+        purplefish3.scale.setTo(.025,.025);
+        // This is a workaround to random movement.
+        purplefish3.body.gravity.y = -200 * Math.random();
+        purplefish3.body.bounce.y = 1;
+        purplefish3.body.gravity.x = -200 * Math.random();
+        purplefish3.body.bounce.y = 1;
         
     }
     
-    function update() {
+    function update() {       
         game.physics.arcade.collide(shark, layer);
         game.physics.arcade.collide(bluefishies, layer);
+        game.physics.arcade.collide(purplefishies, layer);
         game.physics.arcade.collide(shark, bluefishies, killBlue, null, this);
         game.physics.arcade.collide(shark, purplefishies, killPurple, null, this);
         
@@ -101,18 +158,52 @@ window.onload = function() {
         }
         else if (cursors.right.isDown) {
             shark.body.velocity.x = 120;
+            shark.animations.play('right');
         }
         else if (cursors.left.isDown) {
             shark.body.velocity.x = -120;
+            shark.animations.play('left');
         }    
         
     }
     
     function killBlue(shark, bluefish) {
         bluefish.kill();
+        if (shark.animations.play('left')) {
+            shark.animations.play('eatLeft');
+        }
+        else if (shark.animations.play('right')) {
+            shark.animations.play('eatRight');
+        }
     }
     
     function killPurple(shark, purplefish) {
         purplefish.kill();
+        if (shark.animations.play('left')) {
+            shark.animations.play('eatLeft');
+        }
+        else if (shark.animations.play('right')) {
+            shark.animations.play('eatRight');
+        }
     }
+    
+    // To use for improving fish motion at a later date.
+    /*
+    function moveBlue(bluefish, layer) {  
+       
+        if (bluefish.body.touching.left) {
+            bluefish.body.velocity.x *= -1;  
+        }
+        if (bluefish.body.touching.right) {
+            bluefish.body.velocity.x *= -1;  
+        }
+        if (bluefish.body.touching.up) {
+            bluefish.body.velocity.y *= -1;  
+        }
+        if (bluefish.body.touching.down) {
+            bluefish.body.velocity.y *= -1;  
+        }
+        
+    }
+    */
 };
